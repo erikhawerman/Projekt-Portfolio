d44ace2c905d0d353665d5f8efd9693cfcedf7b9
erikhawerman <84011896+erikhawerman@users.noreply.github.com>
2021-09-17 13:20:25 +0200

    slider

--- script.js
+++ script.js
@@ -1,1 +1,59 @@
 "use strict";
+// Slider
+const slides = document.querySelectorAll(".slide");
+const slider = document.querySelector(".slider");
+const btnPause = document.querySelector(".btn-pause");
+let currentSlide = 0;
+const maxSlides = slides.length;
+let sliderRunning = true;
+let run;
+const pausePlayDelay = 500;
+const goToSlide = function (slide) {
+  slides.forEach(
+    (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
+  );
+};
+
+// Next slide
+const nextSlide = function () {
+  if (currentSlide === maxSlides - 1) {
+    currentSlide = 0;
+  } else {
+    currentSlide++;
+  }
+
+  goToSlide(currentSlide);
+};
+
+slider.addEventListener("click", function () {
+  if (sliderRunning) {
+    btnPause.style.display = "block";
+    sliderRunning = false;
+    clearInterval(run);
+    btnPause.classList.contains("play")
+      ? btnPause.classList.remove("play")
+      : "";
+    console.log("hej");
+    btnPause.classList.add("paused");
+    setTimeout(function () {
+      btnPause.style.display = "none";
+    }, pausePlayDelay);
+  } else {
+    btnPause.style.display = "block";
+    setTimeout(function () {
+      btnPause.style.display = "none";
+    }, pausePlayDelay);
+    sliderRunning = true;
+    btnPause.classList.contains("paused")
+      ? btnPause.classList.remove("paused")
+      : "";
+    console.log("test");
+    btnPause.classList.add("play");
+    nextSlide();
+    run = setInterval(nextSlide, 2000);
+  }
+});
+const startTimer = function () {
+  run = setInterval(nextSlide, 2000);
+};
+startTimer();
