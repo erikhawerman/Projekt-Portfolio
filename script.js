d53ae2a792c86087c500f99f1ac09846b02aa7e8
erikhawerman <84011896+erikhawerman@users.noreply.github.com>
2021-09-20 19:55:31 +0200

    hej

--- script.js
+++ script.js
@@ -57,3 +57,163 @@ const startTimer = function () {
   run = setInterval(nextSlide, 2000);
 };
 startTimer();
+
+//Hamburgermeny
+let hamburgerMenyUppe = false;
+
+function hamburgerMenuOpenClose() {
+  console.log("hej");
+  $(".mobile-menu").slideToggle(100, function () {
+    if (!hamburgerMenyUppe) {
+      $(".page-to-dim").fadeTo(100, 0.1);
+      hamburgerMenyUppe = true;
+    } else {
+      $(".page-to-dim").fadeTo(100, 1);
+      hamburgerMenyUppe = false;
+    }
+    $(".menu-list").toggle();
+  });
+}
+$(".burger-icon").click(function () {
+  hamburgerMenuOpenClose();
+});
+
+//Smooth-scrolls mobile
+
+$(".logo").click(function () {
+  $("html, body").animate(
+    {
+      scrollTop: $("#first-page").offset().top,
+    },
+    1000
+  );
+});
+
+$("#startsida").click(function () {
+  $("html, body").animate(
+    {
+      scrollTop: $("#first-page").offset().top,
+    },
+    1000,
+    hamburgerMenuOpenClose()
+  );
+});
+$("#om-oss").click(function () {
+  $("html, body").animate(
+    {
+      scrollTop: $("#second-page").offset().top,
+    },
+    1000,
+    hamburgerMenuOpenClose()
+  );
+});
+$("#vi-jobbar-inom").click(function () {
+  $("html, body").animate(
+    {
+      scrollTop: $("#third-page").offset().top,
+    },
+    1000,
+    hamburgerMenuOpenClose()
+  );
+});
+$("#kontakta-oss").click(function () {
+  $("html, body").animate(
+    {
+      scrollTop: $("#fourth-page").offset().top,
+    },
+    1000,
+    hamburgerMenuOpenClose()
+  );
+});
+
+//Smooth scrolls browser
+
+$(".logo").click(function () {
+  $("html, body").animate(
+    {
+      scrollTop: $("#first-page").offset().top,
+    },
+    1000
+  );
+});
+
+$("#startsida-browser").click(function () {
+  $("html, body").animate(
+    {
+      scrollTop: $("#first-page").offset().top,
+    },
+    1000
+  );
+});
+$("#om-oss-browser").click(function () {
+  $("html, body").animate(
+    {
+      scrollTop: $("#second-page").offset().top,
+    },
+    1000
+  );
+});
+$("#vi-jobbar-inom-browser").click(function () {
+  $("html, body").animate(
+    {
+      scrollTop: $("#third-page").offset().top,
+    },
+    1000
+  );
+});
+$("#kontakta-oss-browser").click(function () {
+  $("html, body").animate(
+    {
+      scrollTop: $("#fourth-page").offset().top,
+    },
+    1000
+  );
+});
+
+//deklarera staplarna som ska fyllas
+const staplar = $(".skillbox-to-fill");
+const procentStapel = function (stapel, procent) {
+  staplar[stapel - 1].procent = procent;
+};
+
+procentStapel(1, 75);
+procentStapel(2, 45);
+procentStapel(3, 95);
+procentStapel(4, 25);
+
+//funktion för att fylla staplarna
+const fyllstapel = function (stapel) {
+  let i = 0;
+  setInterval(function () {
+    if (i >= stapel.procent) return;
+    i++;
+    stapel.style.width = `${i}%`;
+  }, 30);
+};
+
+//Skapa en observer
+const obsCallback = function (entries) {
+  const [entry] = entries;
+  if (entry.isIntersecting) {
+    fyllstapel(entry.target);
+    this.unobserve(entry.target);
+  }
+};
+
+// Observera 2 olika punkter vid samma ställe, med en pixels skillnad för att båda ska köras samtidigt
+const egenskapsObserver = new IntersectionObserver(obsCallback, {
+  root: null,
+  threshold: 0.15,
+});
+const egenskapsObserver2 = new IntersectionObserver(obsCallback, {
+  root: null,
+  threshold: 0.14,
+});
+
+// Fyll staplarna
+Array.from(staplar).forEach(function (egenskap, i) {
+  if (i % 2 === 0) egenskapsObserver.observe(egenskap);
+  else {
+    egenskapsObserver2.observe(egenskap);
+  }
+});
